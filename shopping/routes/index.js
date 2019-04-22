beaf6045c8b8a96489ff57b53395ae12f5bc367f
nguyenhuuhoa <nguyenhuuhoa1998@gmail.com>
2019-04-22 08:03:02 +0700

    Xử lí catogory & index

--- shopping/routes/index.js
+++ shopping/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* GET index.*/
+router.get('/index', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET category. */
+router.get('/category', function(req, res, next) {
+  res.render('category');
 });
 
 module.exports = router;
