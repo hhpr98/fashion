e7258bb3feabae7c2ff9367c1ae9590c6e503fa3
HieuTong <37214275+HieuTong@users.noreply.github.com>
2019-05-07 13:52:35 +0700

    add some *.js for admin

--- admin/app.js
+++ admin/app.js
@@ -6,6 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productRouter = require('./routes/prorduct')
+var productsingleRouter = require('./routes/product-single');
+var productcheckoutRouter = require('./routes/product-checkout');
+var dashboardRouter = require('./routes/dashboard-finance');
+var salesRouter = require('./routes/sales');
+var userRouter = require('./routes/user');
+var editRouter = require('./routes/edit');
+
 
 var app = express();
 
@@ -21,6 +29,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/product', productRouter);
+app.use('/product-single', productsingleRouter);
+app.use('/product-checkout', productcheckoutRouter);
+app.use('/dashboard-finance',dashboardRouter);
+app.use('/sales', salesRouter);
+app.use('/user',userRouter);
+app.use('/edit',editRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- admin/routes/index.js
+++ admin/routes/index.js
@@ -6,39 +6,39 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-/* GET product. */
+/* GET product.
 router.get('/product', function(req, res, next) {
   res.render('product');
 });
-
-/* GET product-single */
+*/
+/* GET product-single
 router.get('/product-single', function(req, res, next) {
   res.render('product-single');
 });
-
-/* GET product-checkout */
+*/
+/* GET product-checkout
 router.get('/product-checkout', function(req, res, next) {
   res.render('product-checkout');
 });
-
-/* GET dashboard-finance */
+*/
+/* GET dashboard-finance
 router.get('/dashboard-finance', function(req, res, next) {
   res.render('dashboard-finance');
 });
-
-/* GET sales */
+*/
+/* GET sales
 router.get('/sales', function(req, res, next) {
   res.render('sales');
 });
-
-/* GET user */
+*/
+/* GET user
 router.get('/user', function(req, res, next) {
   res.render('user');
 });
-
-/* GET edit */
+*/
+/* GET edit
 router.get('/edit', function(req, res, next) {
   res.render('edit');
 });
-
+*/
 module.exports = router;
