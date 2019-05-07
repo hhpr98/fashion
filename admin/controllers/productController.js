1a62c095508f6e9fac0edd9f0ce2ef365c7a85c1
HieuTong <37214275+HieuTong@users.noreply.github.com>
2019-05-07 19:58:36 +0700

    past controller directory from shopping

---
+++ admin/controllers/productController.js
@@ -0,0 +1,41 @@
+var Product = require('../models/product');
+
+// Display list of all Product
+exports.product_list = function(req, res) {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Display detail page for a specific Product.
+exports.product_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED' + req.params.id);
+};
+
+// Display Product create form on GET.
+exports.product_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Handle Product create on POST.
+exports.product_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Display Product delete form on GET.
+exports.product_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Handle Product delete on POST.
+exports.product_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Display Product update form on GET.
+exports.product_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED');
+};
+
+// Handle Product update on POST.
+exports.product_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED');
+};
