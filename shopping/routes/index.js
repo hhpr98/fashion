e9533631eb6a9f6481ebd39e7633e646bd1d21d9
nguyenhuuhoa <nguyenhuuhoa1998@gmail.com>
2019-05-07 20:34:33 +0700

    add controller to router (index.js, adv-find.js, cart.js)

--- shopping/routes/index.js
+++ shopping/routes/index.js
@@ -7,82 +7,40 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+/*
 
-/* GET category.
-router.get('/category', function(req, res, next) {
-  res.render('category');
-});
+// Require controller modules.
+var product_controller = require('../controllers/productController');
+
+/// PRODUCT ROUTES ///
+
+// GET product home page.
+router.get('/', product_controller.index);
+
+// GET request for creating a product. NOTE This must come before routes that display product (uses id).
+router.get('/product/create', product_controller.product_create_get);
+
+// POST request for creating product.
+router.post('/product/create', product_controller.product_create_post);
+
+// GET request to delete product.
+router.get('/product/:id/delete', product_controller.product_delete_get);
+
+// POST request to delete product.
+router.post('/product/:id/delete', product_controller.product_delete_post);
+
+// GET request to update product.
+router.get('/product/:id/update', product_controller.product_update_get);
+
+// POST request to update product.
+router.post('/product/:id/update', product_controller.product_update_post);
+
+// GET request for one product.
+router.get('/product/:id', product_controller.product_detail);
+
+// GET request for list of all product items.
+router.get('/products', product_controller.product_list);
+
 */
 
-/* GET detail product.
-router.get('/single-product', function(req, res, next) {
-  res.render('single-product');
-});
-*/
-
-/* GET checkout
-router.get('/checkout',function (req, res, next) {
-    res.render('checkout');
-})
-*/
-/* GET cart
-router.get('/cart',function (req, res, next) {
-    res.render('cart');
-})
-*/
-/* GET confirmation
-router.get('/confirmation',function (req, res, next) {
-    res.render('confirmation');
-})
-*/
-/* GET blog
-router.get('/blog',function (req, res, next) {
-    res.render('blog');
-})
-*/
-/* GET blog details
-router.get('/single-blog',function (req, res, next) {
-    res.render('single-blog');
-})
-*/
-/* GET login.
-router.get('/login', function(req, res, next) {
-  res.render('login');
-});
-*/
-/* GET register.
-router.get('/registration', function(req, res, next) {
-    res.render('registration');
-});
-*/
-/* GET tracking.
-router.get('/tracking', function(req, res, next) {
-    res.render('tracking');
-});
-*/
-/* GET elements.
-router.get('/elements', function(req, res, next) {
-    res.render('elements');
-});
-*/
-/* GET contact.
-router.get('/contact', function(req, res, next) {
-    res.render('contact');
-});
-*/
-/* GET adv-find.
-router.get('/adv-find', function(req, res, next) {
-    res.render('adv-find');
-});
-*/
-/* GET forgot-password
-router.get('/forgot-password', function(req, res, next) {
-    res.render('forgot-password');
-});
-*/
-/* GET account
-router.get('/account', function(req, res, next) {
-    res.render('account');
-});
-*/
 module.exports = router;
