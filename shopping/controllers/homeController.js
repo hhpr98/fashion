cb8a0d88439616248480c7e6f58e4d78e64c372c
nguyenhuuhoa <nguyenhuuhoa1998@gmail.com>
2019-05-14 21:15:32 +0700

    update database

--- shopping/controllers/homeController.js
+++ shopping/controllers/homeController.js
@@ -1,11 +1,17 @@
 exports.home = (req, res, next) => {
     res.render('/', {
-        allProduct: [{
+        allProduct:[{
             MaSP:1,
             TenSP:'QUẦN JEAN ĐEN QJ1394',
             GiaBan:325000,
-            HinhAnh:'./img/product/quan_jean/QJ1394.jpg',
+            HinhAnh:'./img/product/quan_jean/QJ1394.jpg'
         },
-        ],
+        {
+            MaSP:2,
+            TenSP:'QUẦN JEAN ỐNG ĐỨNG XANH ĐEN QJ1364',
+            GiaBan:150000,
+            HinhAnh:'./img/product/quan_jean/QJ13942.jpg'
+        },
+        ]
     });
 };
--- shopping/routes/index.js
+++ shopping/routes/index.js
@@ -4,13 +4,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{allProduct: [{
-      MaSP:1,
-      TenSP:'QUẦN JEAN ĐEN QJ1394',
-      GiaBan:325000,
-      HinhAnh:'./img/product/quan_jean/QJ1394.jpg',
-    },
-    ]});
+  res.render('index',{
+        allProduct:[{
+          MaSP:1,
+          TenSP:'QUẦN JEAN ĐEN QJ1394',
+          GiaBan:325000,
+          HinhAnh:'./img/product/quan_jean/QJ1394.jpg',
+        },{
+            MaSP:2,
+            TenSP:'QUẦN JEAN ỐNG ĐỨNG XANH ĐEN QJ1364',
+            GiaBan:150000,
+            HinhAnh:'./img/product/quan_jean/QJ1364.jpg',
+          },]
+      }
+    );
 });
 
 module.exports = router;
