bf1bf1242f839e5202b25c7aae66c31ef792b06c
nguyenhuuhoa <nguyenhuuhoa1998@gmail.com>
2019-05-14 17:07:57 +0700

    chỉnh sửa router

--- shopping/routes/account.js
+++ shopping/routes/account.js
@@ -6,40 +6,4 @@ router.get('/', function(req, res, next) {
     res.render('./account/account');
 });
 
-/*
-// Require controller modules.
-var account_controller = require('../controllers/accountController');
-
-/// ACCOUNT ROUTES ///
-
-// GET account home page.
-router.get('/', account_controller.index);
-
-// GET request for creating a account. NOTE This must come before routes that display account (uses id).
-router.get('/account/create', account_controller.account_create_get);
-
-// POST request for creating account.
-router.post('/account/create', account_controller.account_create_post);
-
-// GET request to delete account.
-router.get('/account/:id/delete', account_controller.account_delete_get);
-
-// POST request to delete account.
-router.post('/account/:id/delete', account_controller.account_delete_post);
-
-// GET request to update account.
-router.get('/account/:id/update', account_controller.account_update_get);
-
-// POST request to update account.
-router.post('/account/:id/update', account_controller.account_update_post);
-
-// GET request for one account.
-router.get('/account/:id', account_controller.account_detail);
-
-// GET request for list of all account items.
-router.get('/accounts', account_controller.account_list);
-
-
- */
-
 module.exports = router;
