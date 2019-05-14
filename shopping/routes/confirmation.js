f0713e1320ba5f0f36528fa34b24fbd7a559cc12
nguyenhuuhoa <nguyenhuuhoa1998@gmail.com>
2019-05-14 16:09:57 +0700

    Chỉnh lại cấu trúc thư mục view

--- shopping/routes/confirmation.js
+++ shopping/routes/confirmation.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('./product/confirmation');
+    res.render('./checkout/confirmation');
 });
 
 /*
--- shopping/routes/tracking.js
+++ shopping/routes/tracking.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('tracking');
+    res.render('./information/tracking');
 });
 
 module.exports = router;
