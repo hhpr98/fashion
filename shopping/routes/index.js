8bcd9e754af5e0d9eb216b3c05072f115c8b23e9
nguyenhuuhoa <nguyenhuuhoa1998@gmail.com>
2019-05-14 20:36:43 +0700

    chỉnh cho chạy được, vẫn chưa chạy controller được

--- shopping/routes/index.js
+++ shopping/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-const homeControllers = require('../controllers/homeController');
 
 /* GET home page. */
-router.get('/', homeControllers.home);
+router.get('/', function(req, res, next) {
+  res.render('index',{allProduct: [{
+      MaSP:1,
+      TenSP:'QUẦN JEAN ĐEN QJ1394',
+      GiaBan:325000,
+      HinhAnh:'./img/product/quan_jean/QJ1394.jpg',
+    },
+    ]});
+});
 
 module.exports = router;
