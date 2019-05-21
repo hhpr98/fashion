b8a5ba844874d58a97cbe9d5d3ade13ae1eb6c8e
nguyenhuuhoa <nguyenhuuhoa1998@gmail.com>
2019-05-21 19:38:57 +0700

    Vẫn lỗi chỗ where clause

--- admin/controllers/userController.js
+++ admin/controllers/userController.js
@@ -1,15 +1,8 @@
 const user = require('../models/user');
-var mysql = require('mysql');
-var conn = mysql.createConnection({
-    database: 'fashion_shop',
-    host: "localhost",
-    user: "root",
-    password: ""
-});
 
 exports.userIndex = async (req, res, next) => {
     const userData = await user.allUser();
-    console.log(userData);
+    //console.log(userData);
     res.render('user/index', {userData});
 };
 
@@ -22,19 +15,7 @@ exports.userEdit= async (req, res, next) => {
 };
 
 exports.userDelete=async (req, res, next) => {
-    /*
+
     const userData = await user.deleteUser(res.id);
     res.render('user/index',{userData});
-    */
-
-    conn.connect(function(err) {
-        if (err) throw err;
-
-        var sql = "delete from nguoidung where MaNguoiDung="+res.id.toString();
-        conn.query(sql, function(err) {
-            if (err) throw err;
-        });
-    });
-    const userData = await user.allUser();
-    res.render('user/index', {userData});
 };
--- admin/models/user.js
+++ admin/models/user.js
@@ -1,5 +1,14 @@
 const { dbs } = require('../dbs');
 
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    database: 'fashion_shop',
+    host: "localhost",
+    user: "root",
+    password: ""
+});
+
+
 // get all
 const allUser = async() => {
     return await dbs.user;
@@ -7,7 +16,29 @@ const allUser = async() => {
 exports.allUser=allUser;
 
 // delete by id
+
+var k = {production: {},category:{},user:{}};
 const deleteUser = async(id) => {
+    /*
     return await dbs.user.delete(id);
+    */
+
+    conn.connect(function(err) {
+        if (err) throw err;
+
+        var sql = "delete from nguoidung where MaNguoiDung=" +String(id);
+
+        conn.query(sql, function (err) {
+            if (err) throw err;
+            console.log('Xóa thành công');
+        });
+
+        var sql3 = "select* from nguoidung";
+        conn.query(sql3, function(err, rows) {
+            if (err) throw err;
+            k.user = rows;
+        });
+    });
+    return k.user;
 };
 exports.deleteUser=deleteUser;
