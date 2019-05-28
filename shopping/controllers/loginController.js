3f2a415a1faedaaa09b388c18a21207648220e06
HieuTong <37214275+HieuTong@users.noreply.github.com>
2019-05-28 21:40:18 +0700

    dang nhap thanh cong sp

--- shopping/controllers/loginController.js
+++ shopping/controllers/loginController.js
@@ -3,9 +3,14 @@ const account = require('../models/account');
 exports.accountIndex = async (req, res, next) => {
     const account = await account.allAccount();
     var flag=false;
-    for(a in account){
-        
+    for(i=0;i<account.length;i++){
+        if(account[i].TenDangNhap =="usename" && account[i].MatKhau=="password") {
+            flag = true;
+            break;
 
+        }
     }
-    res.render('account/account', {accountData});
+    if(flag==true){
+        res.render('account/login', {accountData});
+    }
 };
